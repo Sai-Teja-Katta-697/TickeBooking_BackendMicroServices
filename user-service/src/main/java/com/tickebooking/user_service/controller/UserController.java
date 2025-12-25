8376e45dff550a3ae9b8c63f08597f783da25b69
Sai-Teja-Katta-697 <saitejakatta697@gmail.com>
2025-12-25 21:51:40 +0530

    Adding UserService files

---
+++ user-service/src/main/java/com/tickebooking/user_service/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.tickebooking.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tickebooking.user_service.util.Constants.COMMON_ENDPOINT;
+
+@RestController
+@RequestMapping(COMMON_ENDPOINT+"user")
+public class UserController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "User service internal path works";
+    }
+}
